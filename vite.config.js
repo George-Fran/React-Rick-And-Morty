e8e72305d3435cc0779e8bd021ddc3f8ca439e37
Geogz <francogutierrez121@gmail.com>
2023-09-29 20:24:13 -0500

    elpepe

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://George-Fran.github.io/react-rick-y-morty/",
+  base: "https://geogz-rick-and-morty.netlify.app/",
   plugins: [react()]
 })
